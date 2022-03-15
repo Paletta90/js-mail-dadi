ba4b1b751b7b2bd4810ce9b25dfe7fc876673403
DavideDiPietro <davide.dipietro3@gmail.com>
2022-03-15 15:14:03 +0100

    Creo dello stile per l'es MAIL

--- mail/main.js
+++ mail/main.js
@@ -1,21 +1,31 @@
 // Array che contiene le email
 let listMail = ["davide.dipietro3@gmail.com", "andrea.salvatorelli90@hotmail.it", "emanuele.dipietro94@studio.unibo.it"];
-// Variabi che contiene la mail inserita
-let userMail = prompt("Inserisci la tua mail");
+console.log(listMail);
 
+// Variabi che contiene la mail inserita
+let userMail = document.getElementById("email");
+
+//Variabile del button
+let verifica = document.getElementById("verifica");
+
+//Variabile del risultato finale
 let container = document.getElementById("container");
 
-for(i = 0; i <= (listMail.length - 1); i++){
+//Al click faccio uscire la risposta
+verifica.addEventListener('click',
+    function () {
 
-    //Controllo se la mail inserita è presente nell'array. Se si blocco il ciclo
-    if (userMail == listMail[i]){
-        container.innerHTML = `L'email inserita (${userMail}) è presente`;
-        // console.log("Email presente");
-        break;
-    //Se arrivo al controllo dell'ultima posizione senza trovare la mail inserita stampo email non trovata
-    }else if(i == (listMail.length - 1)){
-        container.innerHTML = `L'email inserita (${userMail}) non è presente`;
-        // console.log("Email non presente");
+        for (i = 0; i <= (listMail.length - 1); i++) {
+
+            //Controllo se la mail inserita è presente nell'array. Se si blocco il ciclo
+            if (userMail.value == listMail[i]) {
+                container.innerHTML = `L'email inserita (${userMail.value}) è presente`;
+                break;
+            // Se arrivo al controllo dell'ultima posizione senza trovare la mail inserita stampo email non trovata
+            } else if (i == (listMail.length - 1)) {
+                container.innerHTML = `L'email inserita (${userMail.value}) non è presente`;
+            }
+
+        }
     }
-
-}
+)
